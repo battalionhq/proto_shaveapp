981a5829d9f4870010bb97df65c940ac8bc936b9
danny <dannylindquist@icloud.com>
2015-09-03 18:36:34 -0700

    simple working copy

--- app_driver.py
+++ app_driver.py
@@ -2,4 +2,13 @@ from razor import Razor
 
 myRazor = Razor()
 
-print myRazor.phases
+keyInput = ""
+
+print "Shave App at its simplest"
+print "enter shave to get instruction, enter quit to stop using app."
+
+while keyInput != "quit":
+	keyInput = raw_input(">> ")
+	if keyInput == "shave":
+		myRazor.shave()
+
--- razor.py
+++ razor.py
@@ -3,7 +3,7 @@ class Razor(object):
 	def __init__(self, numPhases=4, usesPerPhase=1):
 		self.phases = numPhases
 		self.usesPerPhase = usesPerPhase
-		self.currentPhase = 1
+		self.currentPhase = 0
 		self.phaseDescriptions = []
 
 	def definePhases(self, phaseNum, description):
@@ -11,14 +11,18 @@ class Razor(object):
 
 
 	def shave(self):
-		if self.currentPhase == 1:
+		if self.currentPhase ==0:
+			print "Choose a side to be side A."
+		elif self.currentPhase == 1:
 			print "Use side B of razor"
 		elif self.currentPhase ==2:
 			print "flip blade and use side A"
 		elif self.currentPhase ==3:
 			print "Use side B of razor"
 		elif self.currentPhase ==4:
+			self.currentPhase = 0
 			print "change blade and start with side A"
 		self.currentPhase +=1
+
 
 			
